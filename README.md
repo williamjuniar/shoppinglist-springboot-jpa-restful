84d7d7527213a00d3d8a31b08db4b172fdfa5f24
William Juniar <williamjuniar@gmail.com>
2021-03-29 12:47:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Demo pembuatan aplikasi sederhana Shopping List dengan menggunakan:
+- Framework: Spring Boot
+- Database Server: PostgreSQL
+- Penyediaan Web Service berbasis RESTful API
+- Web View Template engine: Thymeleaf
+- Build tool: Maven
---
+++ src/main/java/shoppinglist/DemoShoppingListSpringBootApplication.java
@@ -0,0 +1,165 @@
+package shoppinglist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import shoppinglist.entity.DaftarBelanja;
+import shoppinglist.entity.DaftarBelanjaDetil;
+import shoppinglist.repository.DaftarBelanjaRepo;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class DemoShoppingListSpringBootApplication implements CommandLineRunner
+{
+    @Autowired
+    private DaftarBelanjaRepo repo;
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemoShoppingListSpringBootApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+        System.out.println("Membaca Semua Record DaftarBelanja:");
+        List<DaftarBelanja> all = repo.findAll();
+        for (DaftarBelanja db : all) {
+            System.out.println("[" + db.getId() + "] " + db.getJudul());
+
+            List<DaftarBelanjaDetil> listBarang = db.getDaftarBarang();
+            for (DaftarBelanjaDetil barang : listBarang) {
+                System.out.println("\t" + barang.getNamaBarang() + " " + barang.getByk() + " " + barang.getSatuan());
+            }
+        }
+        
+        Scanner keyb = new Scanner(System.in);
+        
+        // Baca berdasarkan ID
+        System.out.print("Masukkan ID dari objek DaftarBelanja yg ingin ditampilkan: ");
+        long id = Long.parseLong(keyb.nextLine());
+        System.out.println("Hasil pencarian: ");
+        
+        Optional<DaftarBelanja> optDB = repo.findById(id);
+        if (optDB.isPresent()) {
+            DaftarBelanja db = optDB.get();
+            System.out.println("\tJudul: " + db.getJudul());
+        }
+        else {
+            System.out.println("\tTIDAK DITEMUKAN.");
+        }
+    }
+    public void CariDaftarBarangBerdasarkanJudul() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Membaca Record DaftarBelanja Berdasarkan Judul:");
+        System.out.print("Masukkan Judul : ");
+        String Judul = scan.nextLine();
+        System.out.println("Hasil pencarian: ");
+        DaftarBelanja tempDb = null;
+        List<DaftarBelanja> all = repo.findAll();
+        for (DaftarBelanja db : all) {
+            if(db.getJudul().equals(Judul)){
+                tempDb = db;
+            }
+        }
+        if(tempDb != null){
+            System.out.println("Judul : " + tempDb.getJudul());
+            List<DaftarBelanjaDetil> listDbd = tempDb.getDaftarBarang();
+            System.out.println("   DetailDaftarBelanja :");
+            for (DaftarBelanjaDetil dbd : listDbd) {
+                System.out.println("\t Nama Barang : " + dbd.getNamaBarang() + "\n\t Jumlah : " +
+                        dbd.getByk() + "\n\t Satuan : " + dbd.getSatuan());
+            }
+            gambarGaris();
+        }else{
+            System.out.println("Data tidak ditemukan!");
+            gambarGaris();
+        }
+    }
+
+    public void TambahDaftarBelanja(){
+        LocalDateTime tglBuat = LocalDateTime.now().withNano(0);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Tambah Record DaftarBelanja");
+        System.out.print("Masukkan Judul : ");
+        String Judul = scan.nextLine();
+        DaftarBelanja db = new DaftarBelanja();
+
+        db.setJudul(Judul);
+        db.setTanggal(tglBuat);
+        repo.save(db);
+        System.out.println("Judul Berhasil Ditambahkan!");
+        gambarGaris();
+    }
+
+    public void PerbaruiDaftarBelanja(){
+        LocalDateTime tglBuat = LocalDateTime.now().withNano(0);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Update Reccord DaftarBelanja:");
+        System.out.print("Masukkan ID : ");
+        String tempId = scan.nextLine();
+        long id = Long.parseLong(tempId);
+        System.out.println("Hasil pencarian: ");
+
+        Optional<DaftarBelanja> optDB = repo.findById(id);
+        if (optDB.isPresent()) {
+            DaftarBelanja db = optDB.get();
+            System.out.println("Judul : " + db.getJudul());
+            List<DaftarBelanjaDetil> listDbd = db.getDaftarBarang();
+            System.out.println("   DetailDaftarBelanja :");
+            for (DaftarBelanjaDetil dbd : listDbd) {
+                System.out.println("\t Nama Barang : " + dbd.getNamaBarang() + "\n\t Jumlah : " +
+                        dbd.getByk() + "\n\t Satuan : " + dbd.getSatuan());
+            }
+            System.out.println("Masukan Data Update");
+            System.out.print("Masukan Judul : ");
+            String Judul = scan.nextLine();
+            db.setJudul(Judul);
+            db.setTanggal(tglBuat);
+            repo.save(db);
+            System.out.println("Berhasil Diupdate");
+            gambarGaris();
+        }
+        else {
+            System.out.println("\tData tidak ditemukan.");
+            gambarGaris();
+        }
+    }
+
+    public void HapusDaftarBelanja(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hapus DaftarBelanja:");
+        System.out.print("Masukkan ID : ");
+        String tempId = scan.nextLine();
+        long id = Long.parseLong(tempId);
+
+        Optional<DaftarBelanja> optDB = repo.findById(id);
+        System.out.println("Data yang ingin dihapus : ");
+        if (optDB.isPresent()) {
+            DaftarBelanja db = optDB.get();
+            System.out.println("Judul : " + db.getJudul());
+            List<DaftarBelanjaDetil> listDbd = db.getDaftarBarang();
+            System.out.println("   DetailDaftarBelanja :");
+            for (DaftarBelanjaDetil dbd : listDbd) {
+                System.out.println("\t Nama Barang : " + dbd.getNamaBarang() + "\n\t Jumlah : " +
+                        dbd.getByk() + "\n\t Satuan : " + dbd.getSatuan());
+            }
+            System.out.println("Yakin ingin menghapus?(y/n)");
+            String del = scan.nextLine();
+            if(del.equals("y")){
+                repo.deleteById(id);
+                System.out.println("Penghapusan Berhasil");
+            }
+            gambarGaris();
+        }
+        else {
+            System.out.println("\tData tidak ditemukan.");
+            gambarGaris();
+        }
+
+    }
+}
